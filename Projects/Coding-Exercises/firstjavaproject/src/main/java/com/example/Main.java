591f837ca4af27e4d605b9f8a499bbf9201b6b57
Benedict Angelo A. Bumatay <benedictangeloaguirre2@gmail.com>
2025-11-13 23:09:39 +0400

    Started java project

---
+++ Projects/Coding-Exercises/firstjavaproject/src/main/java/com/example/Main.java
@@ -0,0 +1,18 @@
+package com.example;
+
+public class Main {
+
+    //This is a comment
+
+    /*And
+     * this
+     * is
+     * a
+     * multi-line
+     * comment
+     */
+    public static void main(String[] args) {
+        System.out.println("Hello world!");
+        System.out.println("This is my first Java project");
+    }
+}
