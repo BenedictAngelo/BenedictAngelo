efd6ba5b02f70107383b0a4c0ec579021781d5fc
Benedict Angelo A. Bumatay <benedictangeloaguirre2@gmail.com>
2025-11-14 08:19:10 +0400

    java

--- Projects/Coding Projects/firstjavaproject/src/main/java/com/example/Main.java
+++ Projects/Coding Projects/firstjavaproject/src/main/java/com/example/Main.java
@@ -1,18 +1,40 @@
 package com.example;
+import java.util.Scanner;
 
 public class Main {
 
-    //This is a comment
-
-    /*And
-     * this
-     * is
-     * a
-     * multi-line
-     * comment
-     */
+    
     public static void main(String[] args) {
         System.out.println("Hello world!");
         System.out.println("This is my first Java project");
+    
+        // MAB LIBS GAME
+
+        Scanner scanner = new Scanner(System.in);
+
+        String adjective1;
+        String noun1;
+        String adjective2;
+        String verb1;
+        String adjective3;
+
+        System.out.println("Enter an adjective (description): ");
+        adjective1 = scanner.nextLine();
+        System.out.println("Enter a noun (animal or person): ");
+        noun1 = scanner.nextLine();
+        System.out.println("Enter an adjective (description): ");
+        adjective2 = scanner.nextLine();
+        System.out.println("Enter a verb end with -ing (action): ");
+        verb1 = scanner.nextLine();
+        System.out.println("Enter an adjective (description): ");
+        adjective3 = scanner.nextLine();
+
+        System.out.println("\nToday I went to a: " + adjective1 + " zoo.");
+        System.out.println("In a exhibit, I saw " + noun1 + ".");
+        System.out.println(noun1 + " was " + adjective2 + " and " + verb1 + "!");
+        System.out.println("I was " + adjective3 + "!");
+        
+        scanner.close();
+
     }
 }
