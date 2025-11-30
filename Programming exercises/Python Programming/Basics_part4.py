a2166d94791b4c904ab8aa307caa1033fff3ca77
Benedict Angelo A. Bumatay <benedictangeloaguirre2@gmail.com>
2025-11-30 18:44:28 +0400

    started python basic 4

---
+++ Programming exercises/Python Programming/Basics_part4.py
@@ -0,0 +1,14 @@
+# indexing - accessing elements of a sequence [ ] (indexing operator)
+# [start:end:stop]
+Name = "Benedict Angelo A. Bumatay"
+
+print(Name[0]) # start only
+
+print(Name[0:8]) # start and end
+        #or
+print(Name[:8])
+
+print(Name[8:])
+
+print(Name[-1]) # starts at the last character
+
