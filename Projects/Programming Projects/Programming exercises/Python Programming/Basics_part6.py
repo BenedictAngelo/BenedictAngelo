470d0eb7f4bfcda4a1d0afaf6e0b5bef3f44123c
Benedict Angelo A. Bumatay <benedictangeloaguirre2@gmail.com>
2025-12-04 15:33:27 +0400

    nothing

--- Projects/Programming Projects/Programming exercises/Python Programming/Basics_part6.py
+++ Projects/Programming Projects/Programming exercises/Python Programming/Basics_part6.py
@@ -1,5 +1,10 @@
 # for loops = execute a block of code a fixed nmber of times
 # can iterate over a range , string, sequence, etc.abs
 
-for x in range(1,11):
+for i in range(1,11):
+    print(i)
+# reverse
+for x in reversed(range(1,11)):
     print(x)
+
+print("Time is up")
