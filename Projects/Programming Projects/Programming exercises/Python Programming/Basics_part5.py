b282e975ec556de61344aed0d3bec5b60bc965a7
Benedict Angelo A. Bumatay <benedictangeloaguirre2@gmail.com>
2025-12-05 10:12:44 +0400

    testing nvim

--- Projects/Programming Projects/Programming exercises/Python Programming/Basics_part5.py
+++ Projects/Programming Projects/Programming exercises/Python Programming/Basics_part5.py
@@ -6,25 +6,25 @@ print("How much is that phone?")
 price1 = 300.545454
 
 print(f"So that phone is {price1}$ ?")
-# make it so that it onlr prints on 2 decimal point
-print(f"Why does it have that {price1:.2f}$ cents?") 
+# make it so that it only prints on 2 decimal point
+print(f"Why does it have that {price1:.2f}$ cents?")
 
 print(f"price:{price1:10}")
 print()
 
 price2 = 200.1234
 price3 = 400.3456786786
-# left allign
+# left align
 print(f"price:{price1:>10}$")
 print(f"price:{price2:>10}$")
 print(f"price:{price3:>10}$")
 print()
-# right allign
+# right align
 print(f"price:{price1:<10}$")
 print(f"price:{price2:<10}$")
 print(f"price:{price3:<10}$")
 print()
-# middle allign
+# middle align
 print(f"price:{price1:^10}$")
 print(f"price:{price2:^10}$")
 print(f"price:{price3:^10}$")
--- Projects/Programming Projects/Programming exercises/Python Programming/Basics_part6.py
+++ Projects/Programming Projects/Programming exercises/Python Programming/Basics_part6.py
@@ -1,10 +1,10 @@
-# for loops = execute a block of code a fixed nmber of times
+# for loops = execute a block of code a fixed number of times
 # can iterate over a range , string, sequence, etc.abs
 
-for i in range(1,11):
+for i in range(1, 11):
     print(i)
 # reverse
-for x in reversed(range(1,11)):
+for x in reversed(range(1, 11)):
     print(x)
 
 print("Time is up")
